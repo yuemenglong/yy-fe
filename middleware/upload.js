914e8e871e9af78f6fb86cec899fed118d2a36e4
yuemenglong <yuemenglong@126.com>
2017-03-22 16:26:52 +0800

    add: upload根据日期分文件夹

--- middleware/upload.js
+++ middleware/upload.js
@@ -28,27 +28,37 @@ function mkdir(path) {
 }
 
 module.exports = function(path) {
-    mkdir(path);
     return function(req, res, next) {
         if (req.method !== "POST") {
             return next();
         }
         var form = new formidable.IncomingForm(); //创建上传表单
+        var dir = moment().format("YYYY-MM-DD");
+        var uploadDir = P.resolve(path, dir);
+        mkdir(uploadDir);
         form.encoding = 'utf-8'; //设置编辑
-        form.uploadDir = path; //设置上传目录
+        form.uploadDir = uploadDir; //设置上传目录
         form.keepExtensions = true; //保留后缀
         form.maxFieldsSize = 8 * 1024 * 1024; //文件大小
 
         form.parse(req, function(err, fields, files) {
             // console.log(files)
-            if (err || !files.file) {
+            if (err) {
                 return res.status(500).json({ name: "UPLOAD_ERROR", message: err.message });
             }
-            var id = moment().format("YYYYMMDD-HHmmss-") + uuid.v4() + P.extname(files.file.name);
-            var newPath = P.resolve(path, "./" + id); //P.resolve([from],to)把to解析为一个绝对路径
-            fs.renameSync(files.file.path, newPath); //重命名（同步版的rename）
-            logger.info("[UPLOAD] %s", id);
-            res.end(id);
+            var file = files.file || files.upfile;
+            if (!file) {
+                return res.status(500).json({ name: "UPLOAD_ERROR", message: "Upload Error, No File Exists" });
+            }
+            var rand = uuid.v4().split("-")[0];
+            var ext = P.extname(file.name);
+            var fileName = new Buffer(P.basename(file.name, ext)).toString("base64");
+            var id = moment().format("YYYYMMDD-HHmmss-") + rand + "-" + fileName + ext;
+            var newPath = P.resolve(uploadDir, id); //P.resolve([from],to)把to解析为一个绝对路径
+            fs.renameSync(file.path, newPath); //重命名（同步版的rename）
+            var ret = `${dir}/${id}`;
+            logger.info("[UPLOAD] %s", ret);
+            res.end(ret);
         });
     }
 }
