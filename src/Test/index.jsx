45f749d2b2792819e81797eed76f731b92e8a6f4
yuemenglong <yuemenglong@126.com>
2017-06-27 19:48:04 +0800

    add: header完成

--- src/Test/index.jsx
+++ src/Test/index.jsx
@@ -1,56 +1,58 @@
 var React = require("react");
 var ev = require("yy-fe/ev");
-var icon = require("./lib/quit.png")
-require("/Test/lib/index");
-
 require("./style.less");
-
-function ThirdClass() {
-    this.fetch = ev.createFetch();
-    this.getInitialState = function() {
-        return { fetchThird: this.fetch("fetchThird", "/fetch-data3") || {} };
-    }
-    this.render = function() {
-        return jade(`h5 Third {this.state.fetchThird.status}`);
-    }
-}
-var Third = React.createClass(new ThirdClass());
-
-function SecondClass() {
-    this.fetch = ev.createFetch();
-    this.getInitialState = function() {
-        return { fetchSecond: this.fetch("fetchSecond", "/fetch-data2") || {} };
-    }
-    this.renderThird = function() {
-        if (!this.state.fetchSecond.status) {
-            return;
-        }
-        return jade(`Third`);
-    }
-    this.render = function() {
-        return jade(`
-        div
-            h3 Second {this.state.fetchSecond.status}
-            |{this.renderThird()}
-            `);
-    }
-}
-var Second = React.createClass(new SecondClass());
+var BG = require("./img/main_01.jpg");
 
 function FirstClass() {
-    this.fetch = ev.createFetch();
     this.getInitialState = function() {
-        return { fetch: this.fetch("fetch", "/fetch-data") || {} };
+        return { navHover: null }
     }
-    this.renderSecond = function() {
-        return jade(`Second`);
+    this.renderHeaderPopup = function() {
+        if (this.state.navHover == null) {
+            return jade(`div(id="header-line")`)
+        } else {
+            return jade(`div(id="header-popup" onMouseOut={this.onNavMouseOut})`)
+        }
+    }
+    this.onNavMouseOver = function(i) {
+        this.setState({ navHover: i })
+    }
+    this.onNavMouseOut = function() {
+        this.setState({ navHover: null })
+    }
+    this.renderNavs = function() {
+        var navs = ["机构", "机构", "机构", "机构", "机构", "机构", "机构"]
+        return jade(`div(id="header-nav")`, navs.map(function(nav, i) {
+            var props = {
+                key: i,
+                onMouseOver: this.onNavMouseOver.bind(null, i),
+                onMouseOut: this.onNavMouseOut,
+            }
+            return jade(`span({...props}) {nav}`)
+        }.bind(this)))
     }
     this.render = function() {
+        var bg = { background: `url("${BG}")` }
         return jade(`
         div
-            pre {JSON.stringify(ev, null, 2)}
-            h1 Test {this.state.fetch.status}
-            |{this.renderSecond()}
+            div(id="top" style={bg})
+                div(id="top-bar")
+                    div(id="top-bar-in")
+                        div(className="left")
+                            div 返回网站首页
+                            div 河南人民政府
+                            div 中国交通运输部
+                        div(className="right")
+                            span 微门户
+                            span 微信公众号
+                            span rss 订阅
+                            span 无障碍版
+                div(id="header")
+                    div(id="header-banner")
+                    div(id="header-search")
+                    |{this.renderNavs()}
+            div(id="body")
+                |{this.renderHeaderPopup()}
             `);
     }
 }
